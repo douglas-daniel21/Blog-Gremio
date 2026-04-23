9af9c7e5779eb0bd65a0ed7ec49e02d7221d7277
name <douglas@email.com>
2026-04-23 16:51:28 -0300

    Atualização 23/04

--- novo-post.php
+++ novo-post.php
@@ -1,10 +1,49 @@
 <?php
+require("validacao.php");
+
 $titulo = $_POST['post_titulo'];
 $resumo = $_POST['post_resumo'];
 $texto = $_POST['post_texto'];
 $autor = $_POST['post_autor'];
 $data = $_POST['post_data'];
 $img = $_POST['post_img'];
+
+$validation = new Validation();
+$data = $validation->validator(
+    [
+    "imdb" => $input["imdb"] ?? "",
+    "titulo" => $input["titulo"] ?? "",
+    "genero" => $input["genero"] ?? [],
+    "duracao" => $input["duracao"] ?? 0,
+    "classificacao" => $input["classificacao"] ?? ""
+    ],//Dados
+
+    [
+        "imdb" => "required|size:10",
+        "titulo" => "required|min:5",
+        "genero" => "required|array|min:1",
+        "duracao" => "required|numeric|min:90",
+        "classificacao" => "required|min:2",
+    ],//Regras
+
+    [
+        "imdb.required" => "O campo imdb é obrigatório.",
+        "titulo.required" => "O campo titulo é obrigatório.",
+        "genero.required" => "O campo genero é obrigatório.",
+        "genero.array" => "O campo genero deve ser um array.",
+        "genero.min" => "O campo genero deve ter pelo menos 1 item.",
+        "duracao.required" => "O campo duracao é obrigatório.",
+        "duracao.integer" => "O campo duracao deve ser um número inteiro.",
+        "classificacao.required" => "O campo classificacao é obrigatório.",
+        "classificacao.string" => "O campo classificacao deve ser uma string."
+    ],//Messages
+);
+
+if($data->fails()){
+    echo json_encode(["success" => false, "message" => $data->errors()->all()]);
+    http_response_code(400);
+    exit;
+}
 
 $sql = "
 INSERT INTO posts (titulo, resumo, texto, autor, data, img)
---
+++ validacao.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Translation\FileLoader; 
+use Illuminate\Translation\Translator;
+use Illuminate\Validation\Factory;
+
+class Validation{
+    private $fileLoader;
+
+    private $translator;
+
+    private $factory;
+
+    public function __construct(){
+        $this->fileLoader = new FileLoader(new Filesystem(),"");
+        $this->translator = new Translator(this->fileLoader,"");
+        $this->factory = new Factory($this->translator);
+    }
+
+    public function validator(array $data, array $rules, array $messages):Validator{
+        return $this->factory->make($data,$rules,$messages)->validate;
+    }
+}
